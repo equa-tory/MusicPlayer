7c3f25cea7fcb8d44131052a762ab2768683e6a0
Equa <TheSuspect9980@gmail.com>
2025-01-22 18:42:22 +0300

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Alex Barauskas on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by Alex Barauskas on 22.01.2025.
+//
+
+import Testing
+
+struct MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
